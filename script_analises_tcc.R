8078dcb213237ae479631c45163c4dc38706e0af
Ludmila <ludmila.silva.resende@gmail.com>
2024-06-30 20:31:13 -0300

    melhoramentos

--- script_analises_tcc.R
+++ script_analises_tcc.R
@@ -1,5 +1,9 @@
 # Script para análise de dados para TCC do MBA em Data Science & Analytics USP/Esalq
 
+
+# Tabela ------------------------------------------------------------------
+
+
 # Objetivo: produzir tabela com produtos dependendentes de polinizadores nas linhas
 # e dados referentes a área plantada, quantidade produzida associada à polinização
 # e valor de produção associado à polinização para o ano de 2022 nas colunas.
@@ -9,13 +13,16 @@ library(tidyverse)
 library(readxl)
 library(writexl)
 
-# Abrir tabelas baixadas diretamente do SIDRA que estão no diretório do projeto:
+# Abrir tabelas baixadas diretamente do SIDRA por meio de API:
 
-dados_area_plant <- read_excel("dados_brasil.xlsx", sheet = 1) # área plantada
-dados_quant_prod <- read_excel("dados_brasil.xlsx", sheet = 2) # quantidade produzida
-dados_valor_prod <- read_excel("dados_brasil.xlsx", sheet = 3) # valor de produção
+cod_area_plant <- "/t/5457/n1/all/v/8331/p/last%2010/c782/all"
+dados_area_plant <- sidrar::get_sidra(api = cod_area_plant)
 
-# Selecionar produtos dependentes de polinizadores:
+cod_quant_prod <- "/t/5457/n1/all/v/214/p/last%2010/c782/all"
+dados_quant_prod <- sidrar::get_sidra(api = cod_quant_prod)
+
+cod_valor_prod <- "/t/5457/n1/all/v/215/p/last%2010/c782/all"
+dados_valor_prod <- sidrar::get_sidra(api = cod_valor_prod)
 
 taxa_dep <- read_excel("dados_TD_v1.xlsx") # abrir tabela com cultivos dependentes de polinizadores e respectivas taxas de dependência
 cult_depend <- taxa_dep$Cultivo # obter nomes dos cultivos
@@ -24,24 +31,22 @@ lista_dados <- list(dados_area_plant, dados_quant_prod, dados_valor_prod) # inpu
 lista_dados_final <- list() # output do loop
 
 for (tabela in lista_dados){
-  dados_final <- as.data.frame(t(tabela)[c(723:794),]) %>% 
-    filter(V4 %in% cult_depend) %>% 
-    select(V5)
+  dados_final <- filter(tabela, Ano == "2022" & `Produto das lavouras temporárias e permanentes` %in% cult_depend)
   lista_dados_final <- append(lista_dados_final, list(dados_final))
 }
 
-tabela <- cbind(taxa_dep, lista_dados_final[c(1:3)]) # montar tabela de resultados
-glimpse(tabela)
-colnames(tabela)[3:5] <- c("area", "quant_prod", "valor_prod")
+tabela <- lista_dados_final[[1]][,c("Produto das lavouras temporárias e permanentes", "Valor")] %>%
+  cbind(lista_dados_final[[2]][,"Valor"], lista_dados_final[[3]][,"Valor"], taxa_dep$TD)
+
+colnames(tabela) <- c("produto", "area", "quant_prod", "valor_prod", "TD")
+
+tabela <- tabela %>% select(produto, TD, everything())
+
 
 # adicionar quantidade produzida e valor de produção associados à polinização (assumindo que há polinização adequada)
 tabela$"quant_prod_dep" <- round(as.numeric(tabela$quant_prod) * as.numeric(tabela$TD), 0)
 tabela$"valor_prod_dep" <- round(as.numeric(tabela$valor_prod) * as.numeric(tabela$TD), 0)
 
-# retirar informação de coco-da-bahía, pois está em mil unidades e não toneladas
-
-coco_da_baia <- as.data.frame(tabela[12,]) # salvar a informação
-tabela <- tabela[-12,] # remover
 
 # adicionar total ao final da tabela
 
@@ -55,68 +60,16 @@ totais <- c("Totais", "-",
 tabela <- rbind(tabela, totais)
 
 write_xlsx(tabela, "prod_2022.xlsx")
-write_xlsx(coco_da_baia, "prod_2022_coco.xlsx")
 
 
+# Gráfico -----------------------------------------------------------------
 
-# Objetivo: obter gráfico de linha para série temporal (11 anos) de área plantada total de
-# produtos dependentes e não-dependentes
+# obter uma tabela que contenha os cultivos nas linhas, uma coluna para cada ano
+# e os valores correspondentes ao total multiplicado pela taxa de dependência preenchendo
+# a tabela
 
-### obter área plantada total
-# tabela era dados_area_plant transposta
-
-# filtrar tabela pelas linhas que contém os anos
-anos <- seq(2012,2022, 1)
-area_plant_total_anos <- tabela %>% filter(str_detect(V3, paste(anos, collapse = "|")))
+# figuras: gráfico de linhas, com anos no eixo X e área no eixo Y
+# agrupar por taxa de dependência
 
 
-### obter área plantada dependente
-
-# retirar linhas que não interessam (linhas com valores totais e 2 primeiras linhas)
-dados_sem_total <- anti_join(tabela, area_plant_total_anos, by = names(tabela))
-dados_sem_total <- dados_sem_total[3:nrow(dados_sem_total),]
-
-# filtrar para os produtos dependentes
-dados_sem_total <- dados_sem_total %>% filter(str_detect(V4, paste(cult_depend, collapse = "|")))
-
-# dividir dados por anos
-tabela_ano <- list()
-
-for (i in 1:11) {
-  inicio <- (i - 1) * 33 + 1
-  fim <- i * 33
-  tabela_ano[[i]] <- dados_sem_total[inicio:fim, ]
-}
-
-names(tabela_ano) <- anos
-
-# calcular a soma da área plantada para cada ano
-soma_area_plantada_dep <- numeric(nrow(area_plant_total_anos))
-
-for (i in 1:length(soma_area_plantada_dep)) {
-  dados_ano <- tabela_ano[[i]]
-  soma <- sum(as.numeric(dados_ano$"V5"), na.rm = T)
-  soma_area_plantada_dep[i] <- soma
-}
-
-# adicionar somas à tabela com dados de produtos não-dependentes
-area_plant_total_anos$"area_plant_dep" <- soma_area_plantada_dep
-
-# organizar tabela
-area_plant_total_anos <- area_plant_total_anos[,-c(1,2,4,6)]
-
-names(area_plant_total_anos) <- c("ano", "area_plant_total", "area_plant_dep")
-
-# calcular a área plantada não-dependente
-
-area_plant_total_anos$area_plant_nao_dep <- as.numeric(area_plant_total_anos$area_plant_total) - as.numeric(area_plant_total_anos$area_plant_dep)
-area_plant_total_anos$perc_dep <- round(as.numeric(area_plant_total_anos$area_plant_dep) / as.numeric(area_plant_total_anos$area_plant_total) * 100, 0)
-area_plant_total_anos$perc_nao_dep <- round(as.numeric(area_plant_total_anos$area_plant_nao_dep) / as.numeric(area_plant_total_anos$area_plant_total) * 100, 0)
-
-# a área plantada de cultivos dependentes de fato está aumentando no Brasil
-# conforme observado por Aizen et al., 2009 (verificar) para o mundo todo
-# entretanto eles se estagnam a partir de 2018
-# talvez seja legal pegar dados de área plantada de todos os anos disponíveis
-# os gráficos de área para produto mostrarão quais são os que mais influenciam
-# nesta tendência
 
